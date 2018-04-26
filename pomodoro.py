05ed0de18f3367b95a66a77b162375ad7702e480
Rene Sanchez <rene.e.sanchez2.civ@mail.mil>
2018-04-26 14:20:44 +0000

    Modify pomodoro.py (WIP)

--- pomodoro.py
+++ pomodoro.py
@@ -39,7 +39,7 @@ def break_between_pomodoros(break_length=5):
             minutes = minutes + 1
         if minutes == break_length:
             os.system('clear')
-            print(f'{break_length} minute break is over!')
+            print(f'{break_length} minute break is over!\n')
             break
         os.system('clear')
         seconds = seconds + 1
@@ -53,10 +53,19 @@ def break_between_pomodoros(break_length=5):
 def main():
     start = input('Enter "s" to start pomodoro timer: ')
     if start.lower() == 's':
-        pomodoro_timer()
-    short_break = input('Enter "b" to start short break: ')
-    if short_break.lower() == 'b':
-        break_between_pomodoros()
+        while True:
+            pomodoro_timer()
+        if pomodoros < 4:
+            short_break = input('Enter "b" to a start short break: ')
+            if short_break.lower() == 'b':
+                break_between_pomodoros()
+        if pomodoros == 4:
+            long_break = input('Enter "b" to start a long break: ')
+            if long_break.lower() == 'b':
+                break_between_pomodoros(20)
+        con = input('Enter "c" to continue and start your next pomodoro: ')
+        if con.lower() == 'c':
+            continue
 
 
 if __name__ == '__main__':
